741250d589553900d8fc5114f90b18e3a6151f10
Angel Jiménez Correa <angel1991huelva@gmail.com>
2023-12-22 20:03:53 +0100

    primero

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+
+
+const SecondsCounter = (props) => {
+  let first = Math.floor(props.seconds / 1) % 10;
+  let second = Math.floor(props.seconds / 10) % 10;
+  let third = Math.floor(props.seconds / 100) % 10;
+  let fourth = Math.floor(props.seconds / 1000) % 10;
+  let fifth = Math.floor(props.seconds / 10000) % 10;
+  let sixth = Math.floor(props.seconds / 100000) % 10;
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+    alert("Han pasado 10 segundos");
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+const contador = () => {
+
+    const [contador, setContador] = useState(0);
+
+    const reiniciar = () => {
+        setContador(0);
+    }
+}
+  return (
+    <div>
+      <div className="bg-light d-flex justify-content-center text-center mt-3 p-5">
+        <div
+          className="bg-success text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          <i className="far fa-clock"></i>
+        </div>
+        <div
+          className="bg-light text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          {sixth}
+        </div>
+        <div
+          className="bg-light text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          {fifth}
+        </div>
+        <div
+          className="bg-light text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          {fourth}
+        </div>
+        <div
+          className="bg-light text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          {third}
+        </div>
+        <div
+          className="bg-light text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          {second}
+        </div>
+        <div
+          className="bg-light text-black mx-2 border border-5 border-dark"
+          style={{ width: "7rem", height: "7rem", fontSize: "4rem" }}
+        >
+          {first}
+        </div>
+      </div>
+      <div className="d-flex justify-content-center">
+        <button className="btn btn-secondary mx-2 mt-3" onClick={reiniciar}> Reiniciar</button>
+        <button className="btn btn-danger mx-2 mt-3">Parar</button>
+      </div>
+    </div>
+  );
+};
+
+export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,35 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+  integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>;
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import SecondsCounter from "./component/SecondsCounter.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let seconds = 0
+
+function incrementSeconds () {
+    seconds += 1;
+    console.log(seconds)
+    //render your react application
+    ReactDOM.render(<SecondsCounter seconds={seconds} />,document.querySelector("#app"));
+}
+
+let myInterval = setInterval(incrementSeconds, 1000);
+
+
+
+
+
+
